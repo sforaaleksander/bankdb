38c8c6f6c1a1bad200a10fdf3cb507f38cb3b146
MateuszGolda <mateusz.golda1993@gmail.com>
2020-10-20 13:41:20 +0200

    reformat code

--- src/main/java/com/codecool/bank_db/App.java
+++ src/main/java/com/codecool/bank_db/App.java
@@ -2,10 +2,8 @@ package com.codecool.bank_db;
 
 import com.codecool.bank_db.tables.CardsGenerator;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         new CardsGenerator(1000).getAvailableIndex();
     }
 }
