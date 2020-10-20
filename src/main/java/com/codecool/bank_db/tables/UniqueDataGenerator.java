753336c1cb3ced54fb59000cf5462b16d0317470
sforaaleksander <sforaaleksander@gmail.com>
2020-10-20 14:11:12 +0200

    make availavleIndexed static

--- src/main/java/com/codecool/bank_db/tables/UniqueDataGenerator.java
+++ src/main/java/com/codecool/bank_db/tables/UniqueDataGenerator.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 public abstract class UniqueDataGenerator extends DataGenerator{
-    protected LinkedList<Integer> availableIndexes;
+    protected static LinkedList<Integer> availableIndexes;
 
     public UniqueDataGenerator(Integer recordCount) {
         super(recordCount);
