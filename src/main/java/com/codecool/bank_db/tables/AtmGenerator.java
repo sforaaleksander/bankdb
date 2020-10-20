bef4579a609500bdd1892d0460c566d8943d1e44
sforaaleksander <sforaaleksander@gmail.com>
2020-10-20 16:18:15 +0200

    implement AtmGenerator

---
+++ src/main/java/com/codecool/bank_db/tables/AtmGenerator.java
@@ -0,0 +1,33 @@
+package com.codecool.bank_db.tables;
+
+import java.util.Random;
+
+public class AtmGenerator extends DataGenerator {
+
+    public AtmGenerator(Integer count) {
+        super(count);
+    }
+
+    @Override
+    public String generate() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        int customerId;
+        int addressId;
+        String isBankProperty;
+        String isActive;
+        for (int i=0; i<recordCount;i++) {
+            if (CustomersGenerator.availableIndexes.isEmpty()){
+                break;
+            }
+            customerId = CustomersGenerator.availableIndexes.poll();
+            addressId = AddressGenerator.availableIndexes.poll();
+            isBankProperty = random.nextInt(10) < 9 ? "true" : "false";
+            isActive = random.nextInt(10) < 9 ? "true" : "false";
+            String command = String.format("insert into atms (%d, %d, %s, %s);\n",
+                    customerId, addressId, isBankProperty, isActive);
+            sb.append(command);
+        }
+            return null;
+    }
+}
