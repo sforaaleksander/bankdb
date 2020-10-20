d6a4e80b9639257d9a986099998d69394a5a8d4d
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-10-20 14:40:12 +0200

    AccountsGenerator add

---
+++ src/main/java/com/codecool/bank_db/tables/AccountsGenerator.java
@@ -0,0 +1,13 @@
+package com.codecool.bank_db.tables;
+
+public class AccountsGenerator extends UniqueDataGenerator{
+
+    public AccountsGenerator(Integer recordCount) {
+        super(recordCount);
+    }
+
+    @Override
+    public String generate() {
+        return null;
+    }
+}
