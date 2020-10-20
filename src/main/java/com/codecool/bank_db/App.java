02941791ca45d070bed2041cbce74574439f3c75
sforaaleksander <sforaaleksander@gmail.com>
2020-10-20 11:08:01 +0200

    create maven project

---
+++ src/main/java/com/codecool/bank_db/App.java
@@ -0,0 +1,13 @@
+package com.codecool.bank_db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
