7306ba7aa10276ef171d6e187bdcfa3e86f4e2f2
sforaaleksander <sforaaleksander@gmail.com>
2020-10-21 17:00:28 +0200

    refactor AccountGenerator.java

--- src/main/java/com/codecool/bank_db/tables/AccountGenerator.java
+++ src/main/java/com/codecool/bank_db/tables/AccountGenerator.java
@@ -1,6 +1,5 @@
 package com.codecool.bank_db.tables;
 
-import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
@@ -23,18 +22,18 @@ public class AccountGenerator extends UniqueDataGenerator {
         LinkedList<String> accountNumbers = generateAccountNumbers();
         int customerId;
         boolean isActive;
-        for (int i=0; i<recordCount;i++) {
-            if (customerGenerator.getAvailableIndexes().isEmpty()){
+        for (int i = 0; i < recordCount; i++) {
+            if (customerGenerator.getAvailableIndexes().isEmpty()) {
                 break;
             }
             customerId = customerGenerator.getAvailableIndexes().poll();
             String accountNo = accountNumbers.poll();
-            long availableBalance = Math.abs(random.nextLong());
-            long bookingBalance = Math.abs(random.nextLong());
-            Timestamp dateOpened = generateDate();
+            long availableBalance = getAvailableBalance(random);
+            long bookingBalance = getBookingBalanceByAvailable(random, availableBalance);
+            Timestamp dateOpened = generateDate(Timestamp.valueOf("2010-01-01 00:00:00"));
             isActive = random.nextInt(11) < 10;
             if (!isActive) {
-                Timestamp dateClosed = generateDate();
+                Timestamp dateClosed = generateDate(dateOpened);
                 String command = String.format("insert into accounts " +
                                 " (customer_id, account_number, available_balance, booking_balance, date_opened, date_closed, is_active) " +
                                 " values (%d, '%s', %d, %d, '%s', '%s', %s);\n",
@@ -52,6 +51,24 @@ public class AccountGenerator extends UniqueDataGenerator {
         return sb.toString();
     }
 
+    private long getBookingBalanceByAvailable(Random random, long availableBalance) {
+        boolean balancesAreEqual = random.nextInt(11) > 9;
+        if (balancesAreEqual) return availableBalance;
+        long difference = availableBalance + (random.nextLong() % availableBalance);
+        return availableBalance + difference;
+    }
+
+    private long getAvailableBalance(Random random) {
+        int richnessIndicator = random.nextInt(1001);
+        if (richnessIndicator == 1000) {
+            return Math.abs(random.nextLong());
+        } else if (richnessIndicator > 900) {
+            return random.nextInt(100_000_000);
+        } else if (richnessIndicator > 600) {
+            return random.nextInt(1_000_000);
+        } else return random.nextInt(100_000);
+    }
+
     private LinkedList<String> generateAccountNumbers() {
         Set<String> numbers = new HashSet<>();
         while (numbers.size() != recordCount) {
@@ -67,10 +84,10 @@ public class AccountGenerator extends UniqueDataGenerator {
         return String.format("%13d", first) + String.format("%13d", second);
     }
 
-    private Timestamp generateDate() {
-        long offset = Timestamp.valueOf("2010-01-01 00:00:00").getTime();
+    private Timestamp generateDate(Timestamp startingDate) {
+        long offset = startingDate.getTime();
         long end = Timestamp.valueOf("2020-10-20 00:00:00").getTime();
         long diff = end - offset + 1;
-        return new Timestamp(offset + (long)(Math.random() * diff));
+        return new Timestamp(offset + (long) (Math.random() * diff));
     }
 }
